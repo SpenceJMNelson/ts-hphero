018bbcc2b4515e5e4625bb68691f2f6f93396d91
Spence Nelson <spenjmn@gmail.com>
2023-07-31 09:53:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("mousemove", mouseMoveFunc);
 let imglayers_s1 = gsap.utils.toArray(".imagelayerframe-1 .mouseparallax");
 let imglayers_s2 = gsap.utils.toArray(".imagelayerframe-2 .mouseparallax");
 let imglayers_s3 = gsap.utils.toArray(".imagelayerframe-3 .mouseparallax");
-let textparallax = gsap.utils.toArray(".hero-text .mouseparallax");
+
 
 function mouseMoveFunc(e) {
 
@@ -62,11 +62,11 @@ function mouseMoveFunc(e) {
 gsap.set(".imagelayerframe-1 .imagewrapper, .imagelayerframe-2 .imagewrapper, .imagelayerframe-3 .imagewrapper",{opacity:0, x:-30})
 
 gsap.timeline({repeat: -1}).to(".imagelayerframe-1 .imagewrapper",{opacity:1, x:0, stagger:0.1, duration:.6, ease:Circ.easeInOut})
-               .to(".imagelayerframe-1 .imagewrapper",{opacity:0, x:60, stagger:0.1, duration:.6, ease:Circ.easeInOut}, "+=3")
+               .to(".imagelayerframe-1 .imagewrapper",{opacity:0, x:60, stagger:0.1, duration:.6, ease:Circ.easeInOut}, "+=5")
                .to(".imagelayerframe-2 .imagewrapper",{opacity:1, x:0, stagger:0.1, duration:.6, ease:Circ.easeInOut}, "-=.3")
-               .to(".imagelayerframe-2 .imagewrapper",{opacity:0, x:60, stagger:0.1, duration:.6, ease:Circ.easeInOut}, "+=3")
+               .to(".imagelayerframe-2 .imagewrapper",{opacity:0, x:60, stagger:0.1, duration:.6, ease:Circ.easeInOut}, "+=5")
                .to(".imagelayerframe-3 .imagewrapper",{opacity:1, x:0, stagger:0.1, duration:.6, ease:Circ.easeInOut}, "-=.3")
-               .to(".imagelayerframe-3 .imagewrapper",{opacity:0, x:60, stagger:0.1, duration:.6, ease:Circ.easeInOut}, "+=3")
+               .to(".imagelayerframe-3 .imagewrapper",{opacity:0, x:60, stagger:0.1, duration:.6, ease:Circ.easeInOut}, "+=5")
 
 gsap.timeline().from(".text-wrap h1 span",{y:100, opacity:0, duration:.6, stagger:0.15, ease:Circ.easeOut})
               .from(".text-wrap .ts-button",{y:30, opacity:0, duration:.6, stagger:0.15, ease:Circ.easeOut}, "-=.5")
